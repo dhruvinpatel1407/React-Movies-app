7fa3ed61953aeae669af0d2d45b7c32e1a008853
dhruvinpatel1407 <bhutdhruvin1407@gmail.com>
2024-11-19 14:24:56 +0530

    moviedetails page updated

--- src/Component/Moviedetails.js
+++ src/Component/Moviedetails.js
@@ -6,7 +6,7 @@ function Moviedetails() {
   const params = useParams();
 
   const filteredData = movies.filter(
-    (movie) => (movie.title === params.movies_Title)
+    (movie) => movie.title === params.movies_Title
   );
 
   return (
@@ -15,33 +15,51 @@ function Moviedetails() {
       {filteredData.map((movie) => {
         return (
           <div className="col s12 m7 bg-white bg-opacity-30">
-            <div className="card flex shadow-lg border border-white border-2 rounded-md">
-              <div className="card-image border border-white border-2 m-8">
-                <img src={movie.big_image} alt={movie.id} className="w-[400px] h-[350px]" />
+            <div className="card flex flex-wrap md:flex-nowrap shadow-lg border border-white border-2 rounded-md">
+              <div className="card-image border border-white border-2 m-4 sm:m-8">
+                <img
+                  src={movie.big_image}
+                  alt={movie.id}
+                  className="w-full sm:w-[300px] md:w-[400px] h-[250px] sm:h-[350px] object-cover"
+                />
               </div>
-              <div className="my-8 mr-8 card-stacked pl-12 bg-white bg-opacity-30">
+              <div className="my-4 sm:my-8 sm:mr-8 card-stacked sm:pl-8 md:pl-12 bg-white bg-opacity-30 flex-1">
                 <div className="card-content">
-                  <h4 className="underline text-white font-bold italic text-2xl">{movie.title}</h4>
-                  <h5 className="text-xl text-white mt-2">Year : {movie.year}</h5>
-                  <h5 className="font-bold text-white underline text-2xl my-4">Description:</h5>
-                  <h5 className="text-xl text-white">{movie.description}</h5>
+                  <h4 className="underline text-white font-bold italic text-lg sm:text-xl md:text-2xl">
+                    {movie.title}
+                  </h4>
+                  <h5 className="text-md sm:text-lg md:text-xl text-white mt-2">
+                    Year : {movie.year}
+                  </h5>
+                  <h5 className="font-bold text-white underline text-lg sm:text-xl md:text-2xl my-4">
+                    Description:
+                  </h5>
+                  <h5 className="text-md sm:text-lg md:text-xl text-white">
+                    {movie.description}
+                  </h5>
                   <ul className="mb-3">
-                    <h5 className="font-bold mt-5 text-white underline text-2xl my-3">Genre:</h5>
+                    <h5 className="font-bold mt-5 text-white underline text-lg sm:text-xl md:text-2xl my-3">
+                      Genre:
+                    </h5>
                     {movie.genre.map((item, index) => (
-                      <li key={index} className="text-white ml-12 text-xl">
+                      <li
+                        key={index}
+                        className="text-white ml-6 sm:ml-8 text-md sm:text-lg md:text-xl"
+                      >
                         <h5>{item}</h5>
                       </li>
                     ))}
                   </ul>
                 </div>
-                <div className="card-action">
+                <div className="card-action mt-4">
                   <a
                     href={movie.imdb_link}
                     target="_blank"
-                  
                     rel="noopener noreferrer"
                   >
-                    <button className="text-white bg-red-900 border border-1 mx-4 p-2 my-2 rounded-lg">WATCH NOW</button>
+                    <button className="text-white bg-red-900 border border-1 mx-2 sm:mx-4 p-2 my-2 rounded-lg">
+                      WATCH NOW
+                    </button>
                   </a>
                 </div>
               </div>
